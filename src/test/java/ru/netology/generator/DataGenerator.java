c6cbc9a844d51f20f2ec4c9237f839e22a83be54
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-11-19 15:30:18 +0200

    methods were added and some were modefited

--- src/test/java/ru/netology/generator/DataGenerator.java
+++ src/test/java/ru/netology/generator/DataGenerator.java
@@ -7,40 +7,57 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class DataGenerator {
-    private static DateTimeFormatter DataTimeFormatter;
-    private Faker faker;
+    private DataGenerator() {
 
+    }
 
     public static String randomDate() {
         Faker faker = new Faker(new Locale("ru"));
         long randomCount = faker.number().numberBetween(1, 9);
-        String randomDate = LocalDate.now().plusDays(3).plusDays(randomCount).format(DataTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String randomDate = LocalDate.now().plusDays(3).plusDays(randomCount).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         return randomDate;
     }
 
-    public static String makeDate(int plusDays) {
+    public static String makeDate() {
         LocalDate today = LocalDate.now();
-        LocalDate newDate = today.plusDays(plusDays);
+        LocalDate newDate = today.plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return formatter.format(newDate);
     }
 
     public static String makeCity() {
-        Faker faker = new Faker(new Locale("ru"));
         String[] myCityList = new String[]{"Абакан", "Анадырь", "Архангельск", "Астрахань", "Барнаул", "Владикавказ", "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Магас", "Махачкала", "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск", "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва", "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону"};
         int city = (int) Math.floor(Math.random() * myCityList.length);
         return myCityList[city];
     }
 
+
     public static String makeName() {
         Faker faker = new Faker(new Locale("ru"));
-        String lastName = faker.name().name();
-        return faker.name().name();
+        return faker.name().lastName() + faker.name().firstName();
+
+    }
+
+    public static String makeInvalidName() {
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().lastName() + faker.name().firstName();
+
     }
 
     public static String makePhone() {
         Faker faker = new Faker(new Locale("ru"));
-        String phone = faker.phoneNumber().phoneNumber();
         return faker.phoneNumber().phoneNumber();
+    }
+
+    public static String makeInvalidOverPhoneNumber() {
+        Faker faker = new Faker(new Locale("ru"));
+        String phone = faker.phoneNumber().phoneNumber();
+        return new String("+7901234567890008");
+    }
+
+    public static String makeInvalidAbovePhoneNumber() {
+        Faker faker = new Faker(new Locale("ru"));
+        String phone = faker.phoneNumber().phoneNumber();
+        return new String("+790123");
     }
 }
